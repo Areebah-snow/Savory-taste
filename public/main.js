5efa3c5e437ab8a8ab97abda77268fc24c3d7b6f
Areebah-snow <Areebah-snow>
2023-05-09 11:15:26 +0100

    Testing testimonials

---
+++ public/main.js
@@ -0,0 +1,9 @@
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: "auto",
+  centeredSlides: true,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
