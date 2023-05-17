a0bc9471afa51cc18eea86f96ac657e2d6151a28
Areebah-snow <Areebah-snow>
2023-05-17 05:59:09 +0100

    Customer reviews

--- public/main.js
+++ public/main.js
@@ -7,3 +7,24 @@ var swiper = new Swiper(".mySwiper", {
     clickable: true,
   },
 });
+
+//testimonial
+
+const userTexts = document.getElementsByClassName("user-text")
+const userPics = document.getElementsByClassName("user-pic")
+
+function showReview(){
+  for(userPic of userPics){
+    userPic.classList.remove("active-pic")
+    userPic.style.scale = '1.2'
+  }
+  for(userText of userTexts){
+    userText.classList.remove("active-text")
+  }
+
+  let i = Array.from(userPics).indexOf(event.target);
+
+  userPics[i].classList.add('active-pic');
+  userTexts[i].classList.add('active-text');
+  userPics[i].style.scale = '1.3'
+}
